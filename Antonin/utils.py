7e4e53972630ab3eeecaeebaf6297fb7758d4c48
Antonin <antonin.huaut@gmail.com>
2019-11-25 13:33:53 +0100

    fonction URF

--- Antonin/utils.py
+++ Antonin/utils.py
@@ -1,15 +1,17 @@
-def expMod(nombre, puissance, modulo): # Exponentiation modulaire
+def expMod(nombre, puissance, modulo):  # Exponentiation modulaire
     p = puissance
     count = 0
-    sumRes = 1
+    sumRes = nombre
+    res = 1
     while p >= 1:
         resMod = p % 2
-        
+
         if resMod == 1:
-            sumRes *= nombre ** (2 ** count)
-            sumRes %= modulo
+            res *= sumRes
+            res %= modulo
 
         p = p // 2
+        sumRes = sumRes ** 2 % modulo
         count += 1
 
-    return sumRes
+    return res
--- testExpoMod.py
+++ testExpoMod.py
@@ -16,19 +16,20 @@ def modpow(base, exp, m):
 def expMod(nombre, puissance, modulo):  # Exponentiation modulaire
     p = puissance
     count = 0
-    sumRes = 1
+    sumRes = nombre
+    res = 1
     while p >= 1:
         resMod = p % 2
 
         if resMod == 1:
-            sumRes *= nombre ** (2 ** count)
-            sumRes %= modulo
+            res *= sumRes
+            res %= modulo
 
         p = p // 2
+        sumRes = sumRes ** 2 % modulo
         count += 1
 
-    return sumRes
-
+    return res
 
 start = time()
 res1 = modpow(10 ^ 999999, 10 ^ 999999, 17)
