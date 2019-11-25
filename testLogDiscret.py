92d48e3e227cd879f57cb58b1acbf470853a2a33
Antonin <antonin.huaut@gmail.com>
2019-11-25 14:26:35 +0100

    log discret

---
+++ testLogDiscret.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("./Clémence")
+from time import *
+from logDiscret import discreteLogarithm
+
+print("Soit une clé publique (661, 53, 566) et une clé secrète 7")
+print("Essayons de trouver la clé secrète à partir de la clé publique via un algorithme de log discret\n")
+start = time()
+res = discreteLogarithm(23, 566, 661) # On trouve la clé secrète qui est 7
+end = time()
+print("Clé secrète trouvée en " + str(end-start) + " ms, et de valeur " + str(res))
+
+# start = time()
+# res = discreteLogarithm(2013, 831, 1073741823)
+# end = time()
+# print(str(res) + " en " + str(end-start) + " ms")
